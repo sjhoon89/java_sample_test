ff83dceadfe885a33c906ce10bd5dc16c8be639b
lsy3709 <lsy3709@naver.com>
2022-10-21 08:33:43 +0900

    duckWorld - 오리 부모, 청둥오리 자식, 행동 인터페이스(날개, 소리) 만들고, 해당 인터페이스를 구현한 클래스에서 메서드 재정의 함. 부모 Duck 클래스에서 해당 인터페이스 타입으로 참조형 변수 설정. 해당 메서드에서 해당 인터페이스형으로 선언된 참조형 변수로 해당 메서드를 호출. MallardDuck 자식 클래스는 (부모 : Duck) 디폴트 생성자에서 부모의  인터페이스형으로 선언된 참조형 변수로 해당 행동 인터페이스를 구현한 클래스 Quack 의 인스턴스를 생성. 날개도 같은 원리. flyBehavior 로 해당 인터페이스를 구현한 클래스 FlyWithWings 를 인스턴스 생성. 추가로 물오리 해봄.

---
+++ src/duckWorld/MiniDuckSimulator.java
@@ -0,0 +1,11 @@
+package duckWorld;
+
+public class MiniDuckSimulator {
+
+	public static void main(String[] args) {
+		Duck mallard = new MallardDuck();
+		mallard.performQuack();
+		mallard.performFly();
+	}
+
+}
