db9cddb82baa081684d995ea4b8ac846173ecee7
lsy3709 <lsy3709@naver.com>
2022-10-24 08:43:22 +0900

    DuckWorld , 로켓오리 추가, 로켓으로 날기, 덕 부모 클래스에, 해당 인터페이스 설정하는 세터부분 추가하기.

--- src/duckWorld/MiniDuckSimulator.java
+++ src/duckWorld/MiniDuckSimulator.java
@@ -17,6 +17,16 @@ public class MiniDuckSimulator {
 		swimDuck.swim();
 		swimDuck.performSwim();
 		
+		//모데 오리 만들기
+		System.out.println("======로켓오리======");
+		Duck model = new ModelDuck();
+		System.out.println("======로켓 오리 변경전===");
+		model.performFly();
+		model.setFlyBehavior(new FlyRocketPowered());
+		System.out.println("======로켓 오리 변경후===");
+		model.performFly();
+		
+		
 	}
 
 }
---
+++ src/duckWorld/ModelDuck.java
@@ -0,0 +1,14 @@
+package duckWorld;
+
+public class ModelDuck extends Duck {
+	public ModelDuck() {
+		flyBehavior = new FlyNoWay();
+		quackBehavior = new Quack();
+	}
+
+	@Override
+	public void display() {
+		System.out.println("저는 모형 오리입니다.");
+	}
+
+}
