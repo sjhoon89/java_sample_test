362af3fa10e4e6a3e2acbc939c1b76513fa94ff1
lsy3709@naver.com <lsy3709@naver.com>
2022-10-24 15:15:23 +0900

    Duck 겹치는 부분 수정.

--- src/duckWorld/Duck.java
+++ src/duckWorld/Duck.java
@@ -3,10 +3,7 @@ package duckWorld;
 public abstract class Duck {
 	FlyBehavior flyBehavior;
 	QuackBehavior quackBehavior;
-<<<<<<< Upstream, based on branch 'master' of https://github.com/lsy3709/java_sample_test.git
 
-=======
->>>>>>> 285dd00 20221024 수업 예제
 	SwimBehavior swimBehavior;
 	
 	public Duck() {}
@@ -23,7 +20,6 @@ public abstract class Duck {
 	
 	public void performSwim() {
 		swimBehavior.swim();
-<<<<<<< Upstream, based on branch 'master' of https://github.com/lsy3709/java_sample_test.git
 	}
 	
 	//메소드 2개 추가. fly 행동 인터페이스, quack 행동 인터페이스 매개변수로 받아 설정하는 
@@ -36,16 +32,10 @@ public abstract class Duck {
 	public void setQuackBehavior(QuackBehavior qb) {
 		quackBehavior = qb;
 		
-=======
->>>>>>> 285dd00 20221024 수업 예제
 	}
 	
 	public void swim () {
-<<<<<<< Upstream, based on branch 'master' of https://github.com/lsy3709/java_sample_test.git
 		System.out.println("모든 오리는 물에 뜹니다. 가짜 오리도 뜨죠");
-=======
-		System.out.println("��� ������ ���� ��ϴ�. ��¥ ������ ����");
->>>>>>> 285dd00 20221024 수업 예제
 	}
 
 }
